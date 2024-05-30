8e34331ca39e9c2d76c43d2f25e50cac68efa21e
Daniel Liu <liudaniel@qq.com>
2024-05-30 15:17:33 +0800

    core: remove unused error from TxPool.Pending (#23720)

--- eth/sync.go
+++ eth/sync.go
@@ -51,7 +51,7 @@ func (pm *ProtocolManager) syncTransactions(p *peer) {
 	//
 	// TODO(karalabe): Figure out if we could get away with random order somehow
 	var txs types.Transactions
-	pending, _ := pm.txpool.Pending(false)
+	pending := pm.txpool.Pending(false)
 	for _, batch := range pending {
 		txs = append(txs, batch...)
 	}
