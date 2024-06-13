73485b7059bce95b7d8afcad6a981208b4d65849
Daniel Liu <liudaniel@qq.com>
2024-06-13 15:50:50 +0800

    core: only check sendernoeoa in non fake mode (#23424)

--- core/types/transaction.go
+++ core/types/transaction.go
@@ -433,7 +433,7 @@ func (tx *Transaction) AsMessage(s Signer, baseFee *big.Int, balanceFee *big.Int
 		amount:          tx.Value(),
 		data:            tx.Data(),
 		accessList:      tx.AccessList(),
-		checkNonce:      true,
+		isFake:          false,
 		balanceTokenFee: balanceFee,
 	}
 	// If baseFee provided, set gasPrice to effectiveGasPrice.
@@ -816,11 +816,11 @@ type Message struct {
 	gasTipCap       *big.Int
 	data            []byte
 	accessList      AccessList
-	checkNonce      bool
+	isFake          bool
 	balanceTokenFee *big.Int
 }
 
-func NewMessage(from common.Address, to *common.Address, nonce uint64, amount *big.Int, gasLimit uint64, gasPrice, gasFeeCap, gasTipCap *big.Int, data []byte, accessList AccessList, checkNonce bool, balanceTokenFee *big.Int, number *big.Int) Message {
+func NewMessage(from common.Address, to *common.Address, nonce uint64, amount *big.Int, gasLimit uint64, gasPrice, gasFeeCap, gasTipCap *big.Int, data []byte, accessList AccessList, isFake bool, balanceTokenFee *big.Int, number *big.Int) Message {
 	if balanceTokenFee != nil {
 		gasPrice = common.GetGasPrice(number)
 	}
@@ -835,7 +835,7 @@ func NewMessage(from common.Address, to *common.Address, nonce uint64, amount *b
 		gasTipCap:       gasTipCap,
 		data:            data,
 		accessList:      accessList,
-		checkNonce:      checkNonce,
+		isFake:          isFake,
 		balanceTokenFee: balanceTokenFee,
 	}
 }
@@ -850,7 +850,7 @@ func (m Message) Value() *big.Int           { return m.amount }
 func (m Message) Gas() uint64               { return m.gasLimit }
 func (m Message) Nonce() uint64             { return m.nonce }
 func (m Message) Data() []byte              { return m.data }
-func (m Message) CheckNonce() bool          { return m.checkNonce }
+func (m Message) IsFake() bool              { return m.isFake }
 func (m Message) AccessList() AccessList    { return m.accessList }
 
 func (m *Message) SetNonce(nonce uint64) { m.nonce = nonce }
